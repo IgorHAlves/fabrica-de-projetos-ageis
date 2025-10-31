03cd2cd299528b3ff404f450f109bd2bf22822eb
Igor Alves <igor.h.alves.ads@gmail.com>
2025-10-31 19:35:53 -0300

    fix: ajustado delete, getbyid e update de produtos

---
+++ API/ECOMMERCE.API/ECOMMERCE.DATA/Migrations/20251030010521_AjustadoDelete.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECOMMERCE.DATA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustadoDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Products_IdPai",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Sale");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Products_IdPai",
+                table: "Products",
+                column: "IdPai",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Products_IdPai",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "Sale",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UserId1 = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sale", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sale_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sale_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sale_UserId",
+                table: "Sale",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sale_UserId1",
+                table: "Sale",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Products_IdPai",
+                table: "Products",
+                column: "IdPai",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
